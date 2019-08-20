cb99624c2453aa03fd801b91e51a5dffcbb6c0d5
super mario <52067274+chinayixia@users.noreply.github.com>
2019-08-20 19:07:42 +0800

    Add files via upload

---
+++ RubikSolverV2/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace RubikSolverV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PortList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BlueToothMessage = new System.Windows.Forms.TextBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.SendMessage = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.StatusMessage = new System.Windows.Forms.TextBox();
+            this.MonitorTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // PortList
+            // 
+            this.PortList.FormattingEnabled = true;
+            this.PortList.Location = new System.Drawing.Point(117, 9);
+            this.PortList.Name = "PortList";
+            this.PortList.Size = new System.Drawing.Size(71, 20);
+            this.PortList.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "选择蓝牙连接端口";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "从NXT读取的数据：";
+            // 
+            // BlueToothMessage
+            // 
+            this.BlueToothMessage.Location = new System.Drawing.Point(15, 66);
+            this.BlueToothMessage.Multiline = true;
+            this.BlueToothMessage.Name = "BlueToothMessage";
+            this.BlueToothMessage.ReadOnly = true;
+            this.BlueToothMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.BlueToothMessage.Size = new System.Drawing.Size(353, 316);
+            this.BlueToothMessage.TabIndex = 3;
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(212, 7);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(75, 21);
+            this.ConnectButton.TabIndex = 4;
+            this.ConnectButton.Text = "连接";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // SendMessage
+            // 
+            this.SendMessage.Location = new System.Drawing.Point(125, 41);
+            this.SendMessage.Name = "SendMessage";
+            this.SendMessage.Size = new System.Drawing.Size(243, 21);
+            this.SendMessage.TabIndex = 5;
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(293, 7);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 21);
+            this.SendButton.TabIndex = 6;
+            this.SendButton.Text = "发送";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // StatusMessage
+            // 
+            this.StatusMessage.Location = new System.Drawing.Point(-3, 390);
+            this.StatusMessage.Name = "StatusMessage";
+            this.StatusMessage.ReadOnly = true;
+            this.StatusMessage.Size = new System.Drawing.Size(682, 21);
+            this.StatusMessage.TabIndex = 12;
+            // 
+            // MonitorTimer
+            // 
+            this.MonitorTimer.Enabled = true;
+            this.MonitorTimer.Interval = 200;
+            this.MonitorTimer.Tick += new System.EventHandler(this.MonitorTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 409);
+            this.Controls.Add(this.StatusMessage);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.SendMessage);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.BlueToothMessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PortList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TaoTao蓝牙PC端";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox PortList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox BlueToothMessage;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.TextBox SendMessage;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.TextBox StatusMessage;
+        private System.Windows.Forms.Timer MonitorTimer;
+    }
+}
+
---
+++ RubikSolverV2/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace RubikSolverV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Get all port list for selection
+            //获得所有的端口列表，并显示在列表内
+            PortList.Items.Clear();
+            string[] Ports = SerialPort.GetPortNames();
+
+            for (int i = 0; i < Ports.Length; i++)
+            {
+                string s = Ports[i].ToUpper();
+                Regex reg = new Regex("[^COM\\d]", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                s = reg.Replace(s, "");
+
+                PortList.Items.Add(s);
+            }
+            if (Ports.Length > 1) PortList.SelectedIndex = 1;
+        }
+
+        #region Bluetooth related codes
+
+        //Create a serial port for bluetooth
+        SerialPort BluetoothConnection = new SerialPort();
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            if (!BluetoothConnection.IsOpen)
+            {
+                //Start
+                Status = "正在连接蓝牙设备";
+                BluetoothConnection = new SerialPort();
+                ConnectButton.Enabled = false;
+                BluetoothConnection.PortName = PortList.SelectedItem.ToString();
+                BluetoothConnection.Open();
+                BluetoothConnection.ReadTimeout = 10000;
+                BluetoothConnection.DataReceived += new SerialDataReceivedEventHandler(BlueToothDataReceived);
+                Status = "蓝牙连接成功";
+            }
+        }
+
+        private void BlueToothDataReceived(object o, SerialDataReceivedEventArgs e)
+        {
+            //int length = BluetoothConnection.ReadByte();
+            Thread.Sleep(1000);
+            int length = 13;
+            BlueToothReceivedData = DateTime.Now.ToLongTimeString() + "\r\n";
+            BlueToothReceivedData += "收到字节数：" + length + "\r\n";
+
+            byte[] data = new byte[length];
+            BluetoothConnection.Read(data,0,length);
+            for (int i = 0; i < length; i++)
+            {
+                BlueToothReceivedData += string.Format("data[{0}] = {1}\r\n", i, data[i]);
+            }
+            //receive close message
+            if (length == 3 && data[0] == 255 && data[1] == 255 && data[2] == 255)
+            {
+                //Stop
+                Status = "正在断开蓝牙设备";
+                BluetoothConnection.Close();
+                BluetoothConnection.Dispose();
+                BluetoothConnection = null;
+                ConnectButton.Enabled = true;
+                Status = "蓝牙断开成功";
+            }
+        }
+
+        private void BlueToothDataSend(byte[] data)
+        {
+            //int length = data.Length;
+            //byte[] readData = new byte[length + 2];
+            //readData[0] = (byte)(length % 255);
+            //readData[1] = (byte)(length / 255);
+            //for (int i = 0; i < length; i++)
+            //{
+            //    readData[i + 2] = data[i];
+            //}
+            //BluetoothConnection.Write(readData, 0, length + 2);
+            BluetoothConnection.Write(data, 0, 1);
+            //Status = "发送数据字节数：" + length;
+        }
+        #endregion
+
+
+        private string Status, BlueToothReceivedData;
+        private void MonitorTimer_Tick(object sender, EventArgs e)
+        {
+            StatusMessage.Text = Status;
+            BlueToothMessage.Text = BlueToothReceivedData;
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            byte n;
+            byte.TryParse(SendMessage.Text, out n);
+
+            BlueToothDataSend(new byte[] { n });
+        }
+    }
+}
+
