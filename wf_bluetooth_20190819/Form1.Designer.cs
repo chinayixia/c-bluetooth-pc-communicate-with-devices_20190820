c1f1fe29e13e3b4ed6551871a4472d5fe0b92713
super mario <52067274+chinayixia@users.noreply.github.com>
2019-08-20 19:09:23 +0800

    add code

---
+++ wf_bluetooth_20190819/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace wf_bluetooth_20190819
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelRecDir = new System.Windows.Forms.Label();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.labelRecInfo = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonListen = new System.Windows.Forms.Button();
+            this.buttonselectRecDir = new System.Windows.Forms.Button();
+            this.buttonSelectFile = new System.Windows.Forms.Button();
+            this.buttonSelectBluetooth = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelRecDir
+            // 
+            this.labelRecDir.AutoSize = true;
+            this.labelRecDir.Location = new System.Drawing.Point(132, 231);
+            this.labelRecDir.Name = "labelRecDir";
+            this.labelRecDir.Size = new System.Drawing.Size(71, 12);
+            this.labelRecDir.TabIndex = 0;
+            this.labelRecDir.Text = "labelRecDir";
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(132, 36);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(77, 12);
+            this.labelAddress.TabIndex = 1;
+            this.labelAddress.Text = "labelAddress";
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(132, 81);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(59, 12);
+            this.labelPath.TabIndex = 2;
+            this.labelPath.Text = "labelPath";
+            // 
+            // labelRecInfo
+            // 
+            this.labelRecInfo.AutoSize = true;
+            this.labelRecInfo.Location = new System.Drawing.Point(132, 260);
+            this.labelRecInfo.Name = "labelRecInfo";
+            this.labelRecInfo.Size = new System.Drawing.Size(77, 12);
+            this.labelRecInfo.TabIndex = 4;
+            this.labelRecInfo.Text = "labelRecInfo";
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(132, 132);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(59, 12);
+            this.labelInfo.TabIndex = 5;
+            this.labelInfo.Text = "labelInfo";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(559, 127);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(146, 23);
+            this.buttonSend.TabIndex = 6;
+            this.buttonSend.Text = "buttonSend";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.ButtonSend_Click);
+            // 
+            // buttonListen
+            // 
+            this.buttonListen.Location = new System.Drawing.Point(559, 255);
+            this.buttonListen.Name = "buttonListen";
+            this.buttonListen.Size = new System.Drawing.Size(146, 23);
+            this.buttonListen.TabIndex = 7;
+            this.buttonListen.Text = "buttonListen";
+            this.buttonListen.UseVisualStyleBackColor = true;
+            this.buttonListen.Click += new System.EventHandler(this.ButtonListen_Click);
+            // 
+            // buttonselectRecDir
+            // 
+            this.buttonselectRecDir.Location = new System.Drawing.Point(559, 226);
+            this.buttonselectRecDir.Name = "buttonselectRecDir";
+            this.buttonselectRecDir.Size = new System.Drawing.Size(146, 23);
+            this.buttonselectRecDir.TabIndex = 8;
+            this.buttonselectRecDir.Text = "buttonselectRecDir";
+            this.buttonselectRecDir.UseVisualStyleBackColor = true;
+            this.buttonselectRecDir.Click += new System.EventHandler(this.ButtonselectRecDir_Click);
+            // 
+            // buttonSelectFile
+            // 
+            this.buttonSelectFile.Location = new System.Drawing.Point(559, 76);
+            this.buttonSelectFile.Name = "buttonSelectFile";
+            this.buttonSelectFile.Size = new System.Drawing.Size(146, 23);
+            this.buttonSelectFile.TabIndex = 9;
+            this.buttonSelectFile.Text = "buttonSelectFile";
+            this.buttonSelectFile.UseVisualStyleBackColor = true;
+            this.buttonSelectFile.Click += new System.EventHandler(this.buttonSelectFile_Click);
+            // 
+            // buttonSelectBluetooth
+            // 
+            this.buttonSelectBluetooth.Location = new System.Drawing.Point(559, 31);
+            this.buttonSelectBluetooth.Name = "buttonSelectBluetooth";
+            this.buttonSelectBluetooth.Size = new System.Drawing.Size(146, 23);
+            this.buttonSelectBluetooth.TabIndex = 10;
+            this.buttonSelectBluetooth.Text = "buttonSelectBluetooth";
+            this.buttonSelectBluetooth.UseVisualStyleBackColor = true;
+            this.buttonSelectBluetooth.Click += new System.EventHandler(this.buttonSelectBluetooth_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSelectBluetooth);
+            this.Controls.Add(this.buttonSelectFile);
+            this.Controls.Add(this.buttonselectRecDir);
+            this.Controls.Add(this.buttonListen);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.labelRecInfo);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.labelRecDir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelRecDir;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.Label labelRecInfo;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonListen;
+        private System.Windows.Forms.Button buttonselectRecDir;
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.Button buttonSelectBluetooth;
+    }
+}
+
---
+++ wf_bluetooth_20190819/Form1.cs
@@ -0,0 +1,169 @@
+﻿using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace wf_bluetooth_20190819
+{
+    public partial class Form1 : Form
+    {
+
+        //https://www.jb51.net/article/82144.htm
+
+
+
+        BluetoothRadio radio = null;//蓝牙适配器 
+        string sendFileName = null;//发送文件名 
+        InTheHand.Net.BluetoothAddress sendAddress = null;//发送目的地址 
+        ObexListener listener = null;//监听器 
+        string recDir = null;//接受文件存放目录 
+        Thread listenThread, sendThread;//发送/接收线程 
+
+        public Form1()
+        {
+            InitializeComponent();
+            radio = BluetoothRadio.PrimaryRadio;//获取当前PC的蓝牙适配器 
+            CheckForIllegalCrossThreadCalls = false;//不检查跨线程调用 
+            if (radio == null)//检查该电脑蓝牙是否可用 
+            {
+                MessageBox.Show("这个电脑蓝牙不可用！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            recDir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            labelRecDir.Text = recDir;
+        }
+
+        private void buttonSelectBluetooth_Click(object sender, EventArgs e)//选择远程蓝牙设备 
+        {
+            InTheHand.Windows.Forms.SelectBluetoothDeviceDialog dialog = new InTheHand.Windows.Forms.SelectBluetoothDeviceDialog();
+            dialog.ShowRemembered = true;//显示已经记住的蓝牙设备 
+            dialog.ShowAuthenticated = true;//显示认证过的蓝牙设备 
+            dialog.ShowUnknown = true;//显示位置蓝牙设备 
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                sendAddress = dialog.SelectedDevice.DeviceAddress;//获取选择的远程蓝牙地址 
+                labelAddress.Text = "地址:" + sendAddress.ToString() + "  设备名:" + dialog.SelectedDevice.DeviceName;
+            }
+        }
+
+        private void buttonSelectFile_Click(object sender, EventArgs e)//选择要发送的本地文件 
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                sendFileName = dialog.FileName;//设置文件名 
+                labelPath.Text = Path.GetFileName(sendFileName);
+            }
+        }
+
+
+               
+        private void ButtonSend_Click(object sender, EventArgs e)
+        {
+            sendThread = new Thread(sendFile);//开启发送文件线程 
+            sendThread.Start();
+        }
+
+        private void sendFile()//发送文件方法 
+        {
+            ObexWebRequest request = new ObexWebRequest(sendAddress, Path.GetFileName(sendFileName));//创建网络请求 
+            WebResponse response = null;
+            try
+            {
+                buttonSend.Enabled = false;
+                request.ReadFile(sendFileName);//发送文件 
+                labelInfo.Text = "开始发送!";
+                response = request.GetResponse();//获取回应 
+                labelInfo.Text = "发送完成!";
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("发送失败！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                labelInfo.Text = "发送失败!";
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    response.Close();
+                    buttonSend.Enabled = true;
+                }
+            }
+        }
+
+
+
+
+
+        private void receiveFile()//收文件方法 
+        {
+            ObexListenerContext context = null;
+            ObexListenerRequest request = null;
+            while (listener.IsListening)
+            {
+                context = listener.GetContext();//获取监听上下文 
+                if (context == null)
+                {
+                    break;
+                }
+                request = context.Request;//获取请求 
+                string uriString = Uri.UnescapeDataString(request.RawUrl);//将uri转换成字符串 
+                string recFileName = recDir + uriString;
+                request.WriteFile(recFileName);//接收文件 
+                labelRecInfo.Text = "收到文件" + uriString.TrimStart(new char[] { '/' });
+            }
+        }
+
+        private void ButtonListen_Click(object sender, EventArgs e)
+        {
+            if (listener == null || !listener.IsListening)
+            {
+                radio.Mode = RadioMode.Discoverable;//设置本地蓝牙可被检测 
+                listener = new ObexListener(ObexTransport.Bluetooth);//创建监听 
+                listener.Start();
+                if (listener.IsListening)
+                {
+                    buttonListen.Text = "停止";
+                    labelRecInfo.Text = "开始监听";
+                    listenThread = new Thread(receiveFile);//开启监听线程 
+                    listenThread.Start();
+                }
+            }
+            else
+            {
+                listener.Stop();
+                buttonListen.Text = "监听";
+                labelRecInfo.Text = "停止监听";
+            }
+        }
+
+        private void ButtonselectRecDir_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择蓝牙接收文件的存放路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                recDir = dialog.SelectedPath;
+                labelRecDir.Text = recDir;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (sendThread != null)
+            {
+                sendThread.Abort();
+            }
+            if (listenThread != null)
+            {
+                listenThread.Abort();
+            }
+            if (listener != null && listener.IsListening)
+            {
+                listener.Stop();
+            }
+        }
+    }
+}
